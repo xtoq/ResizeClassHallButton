19be4afcacab186f573ab837ac2add366f89cc95
Smokie Lee <xtoq@users.noreply.github.com>
2016-09-19 11:50:17 -0500

    comment out potentially unnecessary line

--- ResizeClassHallButton.lua
+++ ResizeClassHallButton.lua
@@ -1,2 +1,2 @@
 --GarrisonLandingPageMinimapButton:ClearAllPoints() -- Unlocks the frame. // TODO: is this necessary?
-GarrisonLandingPageMinimapButton:SetScale(.85) -- Resizes the button to be sort of the size of the other minimap buttons.
+GarrisonLandingPageMinimapButton:SetScale(.85) -- Resizes the button to be sort of the size of the other minimap buttons, given default UI scaling.
