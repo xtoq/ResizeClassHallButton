e66ee3729e0b90c8babdebac47b55432fb6e8c54
Smokie Lee <xtoq@users.noreply.github.com>
2016-09-18 22:37:51 -0500

    initial creation

---
+++ ResizeClassHallButton.lua
@@ -0,0 +1,13 @@
+-- This file is loaded from "ResizeClassHallButton.toc"
+
+GarrisonLandingPageMinimapButton:ClearAllPoints()
+GarrisonLandingPageMinimapButton:SetScale(.85)
+GarrisonLandingPageMinimapButton:SetPoint("center", "UIParent", "center",-872 ,-380)
+GarrisonLandingPageMinimapButton.ignoreFramePositionManager = true
+
+--function resizeMinimapButton()
+--  GarrisonLandingPageMinimapButton:ClearAllPoints();
+--  GarrisonLandingPageMinimapButton:SetScale(.85);
+--  GarrisonLandingPageMinimapButton:SetPoint("center", "UIParent", "center",-872 ,-380);
+--  GarrisonLandingPageMinimapButton.ignoreFramePositionManager = true;
+--end
