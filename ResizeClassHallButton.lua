81c47b04224efac1d3dd9849b8e3ed785b7cc6d9
Smokie Lee <xtoq@users.noreply.github.com>
2016-09-18 23:18:28 -0500

    add comments, removeo unnecessary comments

--- ResizeClassHallButton.lua
+++ ResizeClassHallButton.lua
@@ -1,9 +1,8 @@
--- This file is loaded from "ResizeClassHallButton.toc"
-
-GarrisonLandingPageMinimapButton:ClearAllPoints()
-GarrisonLandingPageMinimapButton:SetScale(.85)
-GarrisonLandingPageMinimapButton:SetPoint("center", "UIParent", "center",-872 ,-380)
-GarrisonLandingPageMinimapButton.ignoreFramePositionManager = true
+GarrisonLandingPageMinimapButton:ClearAllPoints() -- Unlocks the frame. // TODO: is this necessary?
+GarrisonLandingPageMinimapButton:SetScale(.85) -- Resizes the button to be sort of the size of the other minimap buttons.
+GarrisonLandingPageMinimapButton:SetPoint("center","Minimap","center",-80,60) -- Sets the button to the top left of the default minimap.
+--  GarrisonLandingPageMinimapButton:SetPoint("center", "UIParent", "center",-872 ,-380);
+--GarrisonLandingPageMinimapButton.ignoreFramePositionManager = true
 
 --function resizeMinimapButton()
 --  GarrisonLandingPageMinimapButton:ClearAllPoints();
